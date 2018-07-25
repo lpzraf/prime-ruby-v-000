592768faa618c4373773147eec9a1a6ae8d7eab8
Rafael Lopez <lpz.raf@gmail.com>
2018-07-25 00:21:06 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,11 @@
 def prime?(number)
   if number >= 3 
-  (2..number - 1).all? do |x| 
-    number % x != 0 
+    (2..number - 1).all? do |x| 
+      number % x != 0 
+    end
+  else
+    return false
   end
-else
-  return false
-end
 end
 
 
